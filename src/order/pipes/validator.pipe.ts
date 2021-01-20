36c28d2e0531a39405460549823821d87f09a969
Vincent D <42816933+vtdiep@users.noreply.github.com>
2021-01-19 20:27:36 -0800

    Update validator to use order dtos for types

--- src/order/pipes/validator.pipe.ts
+++ src/order/pipes/validator.pipe.ts
@@ -2,20 +2,26 @@ import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import Knex from 'knex';
 import { KnexService } from 'src/common/database/knex/knex.service';
+import { CreateOrderDto } from '../dto/create-order.dto';
+import { OrderDetailDto } from '../dto/order-details.dto';
 
 @Injectable()
 export class ValidatorPipe implements PipeTransform {
 
 constructor(private knexService:KnexService){}
 
-  async transform(value: any, metadata: ArgumentMetadata) {
+  async transform(value: CreateOrderDto, metadata: ArgumentMetadata) {
 
     let knex = this.knexService.knex();
+
+    let items:OrderDetailDto = value.details;
+
+    console.log(items)
+
+    // check that items+modifiers are available and that prices are correct
     let result:Knex<Prisma.orderSelect> = await knex.raw('select * from "order"');
     console.log(result);
-
-    // validate schema?
-    // check menu items available and cost is correct
+    console.log(`value: ${JSON.stringify(value)}`)
     // check total sum is correct
     return value;
   }
