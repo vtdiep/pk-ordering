629ca4b96223bc1ed95923f5c13fd6ddb5c40fb0
Vincent D <42816933+vtdiep@users.noreply.github.com>
2021-01-19 20:28:52 -0800

    Expand draft of mock for validator-pipe test

--- src/order/pipes/validator.pipe.spec.ts
+++ src/order/pipes/validator.pipe.spec.ts
@@ -1,7 +1,18 @@
+import { KnexService } from '../../common/database/knex/knex.service';
 import { ValidatorPipe } from './validator.pipe';
 
 describe('ValidatorPipe', () => {
+  let mockKnexService
+
+  beforeEach( async () => {
+    mockKnexService = {
+      knex: {
+
+      }
+  }
+  })
+
   it('should be defined', () => {
-    expect(new ValidatorPipe()).toBeDefined();
+    expect(new ValidatorPipe(mockKnexService)).toBeDefined();
   });
 });
