332b7ffb7aef43efcd8fc7f07963308fed1358df
Vincent D <42816933+vtdiep@users.noreply.github.com>
2021-06-19 20:55:05 -0700

    test(db): add working concept of db logic test

---
+++ test/database/order.db.spec.ts
@@ -0,0 +1,29 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import Knex from 'knex';
+import { KNEX_CONNECTION } from 'src/common/constants';
+import { KnexModule } from 'src/common/database/knex/knex.module';
+import { KnexService } from 'src/common/database/knex/knex.service';
+
+describe('OrderService', () => {
+  let db: Knex;
+  let module: TestingModule;
+
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      imports: [KnexModule],
+      providers: [],
+    }).compile();
+
+    db = module.get(KNEX_CONNECTION);
+  });
+
+  afterAll(async () => {
+    module.close();
+  });
+
+  beforeEach(async () => {});
+
+  it('should be defined', () => {
+    expect(db).toBeDefined();
+  });
+});
