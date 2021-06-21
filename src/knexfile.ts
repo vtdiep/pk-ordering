7069364b3a2fa541600398a2d40c47d6ed698d28
Vincent D <42816933+vtdiep@users.noreply.github.com>
2021-06-20 23:00:15 -0700

    fix: properly load .env in knexfile  when in prod

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
+import path from 'path';
 
-dotenv.config({ path: '../.env' });
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 // Update with your config settings.
 
