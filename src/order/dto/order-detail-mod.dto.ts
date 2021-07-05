19a3ecd6043e8384f239e47d9440d9ac91764a5f
Vince D <42816933+vtdiep@users.noreply.github.com>
2021-07-05 16:57:46 -0700

    fix(order): avoid cast of boolean to number and enforce array input

--- src/order/dto/order-detail-mod.dto.ts
+++ src/order/dto/order-detail-mod.dto.ts
@@ -1,18 +1,17 @@
 import { Prisma } from '@prisma/client';
 import { Type } from 'class-transformer';
-import { IsInt } from 'class-validator';
+import { IsArray, IsInt } from 'class-validator';
 
 export class OrderDetailModDto {
   [x: string]: Prisma.JsonValue;
 
   @IsInt()
-  @Type(() => Number)
   id: number;
 
+  @IsArray()
   @IsInt({
     each: true,
     message: '$property must be an array of integer numbers',
   })
-  @Type(() => Number)
   modOpts: number[];
 }
