42c18189a76f0542739e950a514af9b66bd3bf7d
Vince D <42816933+vtdiep@users.noreply.github.com>
2021-08-23 21:21:42 -0700

    feat(stripe): add provider for direct access to stripe

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,1 +1,2 @@
 export const KNEX_CONNECTION = 'Knex';
+export const STRIPE_CONNECTION = 'Stripe';
--- src/stripe/stripe.module.ts
+++ src/stripe/stripe.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { STRIPE_CONNECTION } from 'src/common/constants';
 import { StripeService } from './stripe.service';
 import { StripeController } from './stripe.controller';
 
 @Module({
   imports: [ConfigModule],
-  providers: [StripeService],
+  providers: [
+    StripeService,
+    {
+      provide: STRIPE_CONNECTION,
+      useFactory: async (stripeService) => stripeService.getStripe(),
+      inject: [StripeService],
+    },
+  ],
   controllers: [StripeController],
+  exports: [STRIPE_CONNECTION, StripeService],
 })
 export class StripeModule {}
