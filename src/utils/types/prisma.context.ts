930b7d5c7c9cd64674a956d03a6e3a380a95b4e9
Vince D <42816933+vtdiep@users.noreply.github.com>
2021-08-24 21:23:31 -0700

    fix(hotfix, test): update mock prisma to use proper mock context
    Since v2.0.0 of jest-mock-extended, DeepMockProxy should to be used instead of
    MockProxy for mocking prisma.
    ! However, since https://github.com/marchaos/jest-mock-extended/pull/72
    has not yet been officially released, we need to manually export DeepMockProxy
    inside of the jest-mock-extended library definitions

--- src/utils/types/prisma.context.ts
+++ src/utils/types/prisma.context.ts
@@ -1,12 +1,12 @@
 import { PrismaClient } from '@prisma/client';
-import { MockProxy, mockDeep, mock } from 'jest-mock-extended';
+import { DeepMockProxy, mockDeep, mock } from 'jest-mock-extended';
 
 export type Context = {
   prisma: PrismaClient;
 };
 
 export type MockContext = {
-  prisma: MockProxy<PrismaClient>;
+  prisma: DeepMockProxy<PrismaClient>;
 };
 
 export const createMockContext = (): MockContext => ({
