0b34c020ab57510f44d6c9d57dd43585dda7cb5e
Vince D <42816933+vtdiep@users.noreply.github.com>
2021-09-14 20:00:40 -0700

    test(order): add test and fix missing dependencies

--- src/order/order.controller.spec.ts
+++ src/order/order.controller.spec.ts
@@ -1,6 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { KNEX_CONNECTION } from 'src/common/constants';
+import { makeMockOrder } from './fixtures/mockData';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
+import { ValidatorPipe } from './pipes/validator.pipe';
+
+const defaultRedirectURL = 'http://example.com';
 
 describe('OrderController', () => {
   let controller: OrderController;
@@ -8,7 +13,22 @@ describe('OrderController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OrderController],
-      providers: [OrderService],
+      providers: [
+        {
+          provide: OrderService,
+          useValue: {
+            create: jest.fn().mockReturnValue({ url: 'http://example.com' }),
+          },
+        },
+        {
+          provide: ValidatorPipe,
+          useValue: {},
+        },
+        {
+          provide: KNEX_CONNECTION,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     controller = module.get<OrderController>(OrderController);
@@ -16,5 +36,14 @@ describe('OrderController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should return with the redirect url', async () => {
+      const redirectURL = await controller.create(makeMockOrder());
+      console.log(redirectURL);
+
+      expect(redirectURL.url).toBe(defaultRedirectURL);
+    });
   });
 });
