b756eaf997ed45b41eee6f069b62a31785202b5e
Vince D <42816933+vtdiep@users.noreply.github.com>
2021-09-14 20:03:53 -0700

    test(auth): add tests and fix missing dependencies

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { knex } from 'knex';
+import { getTracker, MockClient, Tracker } from 'knex-mock-client';
+import { KNEX_CONNECTION } from 'src/common/constants';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let mockKnexClient: Tracker;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: KNEX_CONNECTION,
+          useValue: knex({ client: MockClient }),
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    mockKnexClient = getTracker();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return username upon valid username/pw combo', async () => {
+    // mockKnexClient.on()
+    let res = await service.validateUser('abc', '123');
+    expect(res).toEqual({ username: 'abc' });
+  });
+
+  it('should return nothing upon invalid username/pw combo', async () => {
+    // mockKnexClient.on()
+    let res = await service.validateUser('abc', '000');
+    expect(res).toBeNull();
   });
 });
