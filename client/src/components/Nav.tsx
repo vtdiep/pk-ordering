9fb96e8fd287ebab4bfb1155e8de24edb2aa6399
Vince D <42816933+vtdiep@users.noreply.github.com>
2021-09-21 16:46:09 -0700

    refactor(client): make nav responsive

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -2,22 +2,51 @@ import styled from 'styled-components';
 
 const StyledNav = styled.nav`
   grid-column: 1 / span 4;
+  border-top: 1px solid grey;
+  border-bottom: 1px solid grey;
+  padding-top: 5px;
+  padding-bottom: 5px;
 `;
 
 const StyledUL = styled.ul`
   display: flex;
   list-style-type: none;
   column-gap: 20px;
-  background-color: red;
+  background-color: #e4e4e4;
+
+  margin-left: auto;
+  margin-right: auto;
+  padding: 10px 10px 0 10px;
+
+  & li {
+    display: flex;
+    flex-direction: row;
+    align-items: baseline;
+  }
+
+  @media (min-width: 640px) {
+    /* max size of card + card gutter */
+    max-width: calc(640px * 2 + 20px);
+  }
+`;
+
+const ULContainer = styled.div`
+  @media (min-width: 640px) {
+    margin-left: 20px;
+    margin-right: 20px;
+  }
 `;
 
 export const Nav = () => {
   return (
     <StyledNav>
-      <StyledUL>
-        <li>Nav1</li>
-        <li>Nav2</li>
-      </StyledUL>
+      <ULContainer>
+        <StyledUL>
+          <li>Navigation Text</li>
+          <li>Nav2</li>
+          <li>Navigation Text Long Long Long</li>
+        </StyledUL>
+      </ULContainer>
     </StyledNav>
   );
 };
