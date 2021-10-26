9f19c894801f0b1642c4aaa1bda1342a7bacf1f8
Vince D <42816933+vtdiep@users.noreply.github.com>
2021-10-26 16:43:22 -0700

    feat(store): implement store query to database

--- src/store/entities/store.entity.ts
+++ src/store/entities/store.entity.ts
@@ -1,1 +1,49 @@
-export class Store {}
+/* eslint-disable max-classes-per-file */
+export class StoreCategoryItemRef {
+  cxi_do: number;
+
+  item_id: number;
+}
+
+export class StoreCategory {
+  items: StoreCategoryItemRef[];
+
+  category_id: number;
+
+  category_name: string | null;
+
+  category_active: boolean;
+
+  category_description: string | null;
+
+  mxc_display_order: number;
+}
+
+export class StoreMenu {
+  menu_id: number;
+
+  menu_name: string | null;
+
+  menu_display_order: number;
+
+  menu_description: string | null;
+
+  category: StoreCategory[];
+}
+
+export class StoreItem {
+  item_id: number;
+
+  name: string;
+
+  description: string | null;
+
+  price: string | null;
+}
+
+export class StoreData {
+  data: {
+    menus: StoreMenu[];
+    items: StoreItem[];
+  };
+}
--- src/store/store.controller.ts
+++ src/store/store.controller.ts
@@ -15,28 +15,28 @@ import { UpdateStoreDto } from './dto/update-store.dto';
 export class StoreController {
   constructor(private readonly storeService: StoreService) {}
 
-  @Post()
-  create(@Body() createStoreDto: CreateStoreDto) {
-    return this.storeService.create(createStoreDto);
-  }
+  // @Post()
+  // create(@Body() createStoreDto: CreateStoreDto) {
+  //   return this.storeService.create(createStoreDto);
+  // }
 
   @Get()
   findAll() {
     return this.storeService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.storeService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.storeService.findOne(+id);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateStoreDto: UpdateStoreDto) {
-    return this.storeService.update(+id, updateStoreDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateStoreDto: UpdateStoreDto) {
+  //   return this.storeService.update(+id, updateStoreDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.storeService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.storeService.remove(+id);
+  // }
 }
