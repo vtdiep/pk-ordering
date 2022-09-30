8eeb625ed82669e1d5e1723748016426ad53390d
Vince D <42816933+vtdiep@users.noreply.github.com>
2022-09-30 14:04:40 -0700

    style(client): fix formatting

--- client/src/components/ItemModal.tsx
+++ client/src/components/ItemModal.tsx
@@ -1,7 +1,7 @@
-import styled from "styled-components";
-import { useNavigate, useParams } from "react-router-dom";
-import { ItemModalProps } from "../types/ItemModelProps";
-import { useEffect } from "react";
+import styled from 'styled-components';
+import { useNavigate, useParams } from 'react-router-dom';
+import { ItemModalProps } from '../types/ItemModelProps';
+import { useEffect } from 'react';
 
 const StyledDiv = styled.div`
   position: fixed;
@@ -13,26 +13,29 @@ const StyledDiv = styled.div`
   right: 0;
   bottom: 0;
   border: 0px;
-    background-color: rgba(91, 112, 131, 0.4);
-`
+  background-color: rgba(91, 112, 131, 0.4);
+`;
 
-export const ItemModal = (props:ItemModalProps) =>{
+export const ItemModal = (props: ItemModalProps) => {
+  useEffect(() => {
+    document.body.style.overflowY = 'hidden';
+  }, []);
 
-    useEffect( () => {
-        document.body.style.overflowY = 'hidden';
-    }, [])
+  let navigate = useNavigate();
+  let { name } = useParams();
+  // let name = props.name
 
-    let navigate = useNavigate()
-    let {name} = useParams()
-    // let name = props.name
-
-    return (
-        <StyledDiv>
-            {name}
-            <span onClick={(e:any) =>{
-                document.body.style.overflow = 'unset';
-                navigate('/') }
-                }>Close</span>
-        </StyledDiv>
-    )
-}
+  return (
+    <StyledDiv>
+      {name}
+      <span
+        onClick={(e: any) => {
+          document.body.style.overflow = 'unset';
+          navigate('/');
+        }}
+      >
+        Close
+      </span>
+    </StyledDiv>
+  );
+};
