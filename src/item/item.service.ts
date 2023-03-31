b2795bf60c48aba21b03d11231e6f86c2546a9f0
Vincent Diep <42816933+vtdiep@users.noreply.github.com>
2023-03-30 21:43:51 -0700

    feat(server): allow some mod data in item query

--- src/item/item.service.ts
+++ src/item/item.service.ts
@@ -17,12 +17,25 @@ export class ItemService {
     return this.prismaCtx.prisma.item.findMany();
   }
 
-  findOne(id: number) {
-    return this.prismaCtx.prisma.item.findUnique({
+  findOne(id: number, includeMods: boolean = false) {
+    let conditions = {
       where: {
         item_id: id,
       },
-    });
+      include: includeMods
+        ? {
+            item_X_modgroup: {
+              select: {
+                mod_id: true,
+                display_order: true,
+              },
+            },
+          }
+        : {},
+    };
+    let query = this.prismaCtx.prisma.item.findUnique(conditions);
+
+    return query;
   }
 
   update(id: number, updateItemDto: Prisma.itemUpdateInput) {
