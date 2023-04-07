b92452d34a95740077ac897b26b53bfe94b60d0d
Vincent Diep <42816933+vtdiep@users.noreply.github.com>
2023-04-06 21:18:46 -0700

    fix(item): fix argument for empty prisma condition

--- src/item/item.service.ts
+++ src/item/item.service.ts
@@ -31,7 +31,7 @@ export class ItemService {
               },
             },
           }
-        : {},
+        : null,
     };
     let query = this.prismaCtx.prisma.item.findUnique(conditions);
 
