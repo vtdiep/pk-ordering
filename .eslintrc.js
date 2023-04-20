8b498104f12df30cf9a3164715f12ea904c342b0
Vincent Diep <42816933+vtdiep@users.noreply.github.com>
2023-04-19 18:29:22 -0700

    chore(eslint): add eslint-plugin-jest for tests
    apply jest linting rules to spec test files

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,12 +29,29 @@ module.exports = {
     '@typescript-eslint/no-use-before-define': 'off',
     'import/no-extraneous-dependencies': [
       'error', {'devDependencies': ['**/*.test.ts', '**/*.spec.ts', '**/*-spec.ts', './src/utils/**/**.ts', '**/mock*.ts']}
-    ],  
+    ],
   },
   "overrides": [{
     "files": ["*.spec.ts"],
     "rules": {
       'no-param-reassign': ['error', { props: true, ignorePropertyModificationsForRegex: ["^draft"] }],
     }
-  }]
+  },
+  { "files": ["{src,apps,libs,test}/**/*.spec.ts"],
+    "plugins": ["jest"],
+    "rules": {
+      "jest/no-identical-title": "error",
+      "jest/prefer-to-have-length": "warn",
+      "jest/valid-expect": "error",
+      "jest/expect-expect": "error"    
+      // "jest/no-disabled-tests": "warn",
+      // "jest/no-focused-tests": "error",
+    }
+  }
+],
+  // settings: {
+  //   react: {
+  //    version: "999",
+  //   },
+  // }
 };
