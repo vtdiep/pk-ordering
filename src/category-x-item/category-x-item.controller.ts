2395e5be2b67e53de87f6dad7381f5c1af4f4be8
Vincent Diep <42816933+vtdiep@users.noreply.github.com>
2023-04-30 00:45:06 -0700

    feat(category-x-item): add error handling & class serializer for dtos

--- src/category-x-item/category-x-item.controller.ts
+++ src/category-x-item/category-x-item.controller.ts
@@ -7,21 +7,46 @@ import {
   Param,
   Delete,
   UseFilters,
+  ClassSerializerInterceptor,
+  UseInterceptors,
+  NotFoundException,
+  BadRequestException,
 } from '@nestjs/common';
-import { category_X_item } from '@prisma/client';
+import { Prisma, category_X_item } from '@prisma/client';
+import { plainToClass } from 'class-transformer';
 import { CategoryXItemService } from './category-x-item.service';
 import { CreateCategoryXItemDto } from './dto/create-category-x-item.dto';
 import { UpdateCategoryXItemDto } from './dto/update-category-x-item.dto';
+import { CategoryXItem } from './entities/category-x-item.entity';
 
+@UseInterceptors(ClassSerializerInterceptor)
 @Controller('category-item')
 export class CategoryXItemController {
   constructor(private readonly categoryXItemService: CategoryXItemService) {}
 
   @Post()
   async create(@Body() createCategoryXItemDto: CreateCategoryXItemDto) {
-    let result: category_X_item;
-    result = await this.categoryXItemService.create(createCategoryXItemDto);
-    return result;
+    let result: category_X_item | null = null;
+    try {
+      result = await this.categoryXItemService.create(createCategoryXItemDto);
+      console.log(result);
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        // The .code property can be accessed in a type-safe manner
+        if (e.code === 'P2002') {
+          console.log(
+            'There is a unique constraint violation, a new category-x-item cannot be created',
+          );
+          throw new BadRequestException();
+        }
+        throw new BadRequestException();
+      }
+    }
+    if (result == null) {
+      throw new BadRequestException();
+    }
+
+    return plainToClass(CategoryXItem, result);
   }
 
   @Get()
@@ -30,18 +55,22 @@ export class CategoryXItemController {
     try {
       result = await this.categoryXItemService.findAll();
     } catch (error) {
-      console.log(error);
+      throw new BadRequestException();
     }
     console.log(result);
 
-    return result;
+    // eslint-disable-next-line arrow-body-style
+    return result.map((entry) => {
+      return plainToClass(CategoryXItem, entry);
+    });
   }
 
   @Get(':cid/:iid')
   async findOne(@Param('cid') cid: string, @Param('iid') iid: string) {
     let result: category_X_item | null;
     result = await this.categoryXItemService.findOne(+cid, +iid);
-    return result;
+    if (result == null) throw new NotFoundException();
+    return plainToClass(CategoryXItem, result);
   }
 
   @Put(':cid/:iid')
@@ -56,13 +85,13 @@ export class CategoryXItemController {
       +iid,
       updateCategoryXItemDto,
     );
-    return result;
+    return plainToClass(CategoryXItem, result);
   }
 
   @Delete(':cid/:iid')
   async remove(@Param('cid') cid: string, @Param('iid') iid: string) {
     let result: category_X_item;
     result = await this.categoryXItemService.remove(+cid, +iid);
-    return result;
+    return plainToClass(CategoryXItem, result);
   }
 }
