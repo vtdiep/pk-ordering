f006d5c2adc0b5a2bc854f67cfe7b57d43ae9df7
Vincent Diep <42816933+vtdiep@users.noreply.github.com>
2023-05-06 20:19:12 -0700

    refactor(menu-x-category): use ClassSerializer to transform & sanitize data

--- src/menu-x-category/menu-x-category.controller.ts
+++ src/menu-x-category/menu-x-category.controller.ts
@@ -3,16 +3,21 @@ import {
   Get,
   Post,
   Body,
-  Put,
   Param,
   Delete,
   Patch,
+  ClassSerializerInterceptor,
+  UseInterceptors,
+  Query,
 } from '@nestjs/common';
 import { menu_X_category } from '@prisma/client';
+import { plainToClass } from 'class-transformer';
 import { MenuXCategoryService } from './menu-x-category.service';
 import { CreateMenuXCategoryDto } from './dto/create-menu-x-category.dto';
 import { UpdateMenuXCategoryDto } from './dto/update-menu-x-category.dto';
+import { MenuXCategory } from './entities/menu-x-category.entity';
 
+@UseInterceptors(ClassSerializerInterceptor)
 @Controller('menu-x-category')
 export class MenuXCategoryController {
   constructor(private readonly menuXCategoryService: MenuXCategoryService) {}
@@ -21,27 +26,27 @@ export class MenuXCategoryController {
   async create(@Body() createMenuXCategoryDto: CreateMenuXCategoryDto) {
     let result: menu_X_category;
     result = await this.menuXCategoryService.create(createMenuXCategoryDto);
-    return result;
+    return plainToClass(MenuXCategory, result);
   }
 
   @Get()
   async findAll() {
     let result: menu_X_category[];
     result = await this.menuXCategoryService.findAll();
-    return result;
+    return result.map((mxc) => plainToClass(MenuXCategory, mxc));
   }
 
   @Get(':mid/:cid')
   async findOne(@Param('mid') mid: string, @Param('cid') cid: string) {
     let result: menu_X_category | null;
     result = await this.menuXCategoryService.findOne(+mid, +cid);
-    return result;
+    return plainToClass(MenuXCategory, result);
   }
 
-  @Patch(':mid/:cid')
+  @Patch()
   async update(
-    @Param('mid') mid: string,
-    @Param('cid') cid: string,
+    @Query('mid') mid: string,
+    @Query('cid') cid: string,
     @Body() updateMenuXCategoryDto: UpdateMenuXCategoryDto,
   ) {
     let result: menu_X_category;
@@ -50,13 +55,13 @@ export class MenuXCategoryController {
       +cid,
       updateMenuXCategoryDto,
     );
-    return result;
+    return plainToClass(MenuXCategory, result);
   }
 
   @Delete(':mid/:cid')
   async remove(@Param('mid') mid: string, @Param('cid') cid: string) {
     let result: menu_X_category;
     result = await this.menuXCategoryService.remove(+mid, +cid);
-    return result;
+    return plainToClass(MenuXCategory, result);
   }
 }
